9eed52bc3d81f2233460c78af3bd676789e2d885
Yura11 <y.belichenko007@gmail.com>
2024-05-26 17:04:31 +0300

    delete regions in nat

--- terraform/modules/nat/main.tf
+++ terraform/modules/nat/main.tf
@@ -2,7 +2,6 @@
 
 resource "google_compute_router" "router" {
   name = "my-router"
-  ##region  = google_compute_subnetwork.default.region
   network = var.vpc_id
 
   bgp {
@@ -13,7 +12,6 @@ resource "google_compute_router" "router" {
 resource "google_compute_router_nat" "nat" {
   name   = "my-router-nat"
   router = google_compute_router.router.name
-  ##region                             = google_compute_router.router.region
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 
