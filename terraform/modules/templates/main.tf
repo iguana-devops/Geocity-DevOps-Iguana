acb77c99bf8319e276822f420e87380e01340383
Yura11 <y.belichenko007@gmail.com>
2024-05-27 16:46:31 +0300

    added ssh-keys to vm's

--- terraform/modules/templates/main.tf
+++ terraform/modules/templates/main.tf
@@ -18,8 +18,15 @@ resource "google_compute_instance_template" "instance_template" {
   }
 
   tags = ["allow-ssh", "load-balanced-backend"]
+  
+  metadata = {
+    ssh-keys = "ubuntu:${data.google_secret_manager_secret_version.ssh_key.secret_data}"
+  }
 }
 
+data "google_secret_manager_secret_version" "ssh_key" {
+  secret      = "rsa_pub"
+}
 data "google_compute_image" "ubuntu" {
   family  = "ubuntu-2204-lts"
   project = "ubuntu-os-cloud"
