7a5a28940dd9a6aeb315b65dedce7c6c0e75af98
Yura11 <y.belichenko007@gmail.com>
2024-06-01 14:23:50 +0300

    deleted refactored dir

--- refactored_terraform/geo_tf/modules.tf
+++
@@ -1,8 +0,0 @@
-module "monitoring-prometheus" {
-  source       = "../modules/monitoring-prometheus"
-  network      = google_compute_network.geo-network.id
-  subnetwork   = google_compute_subnetwork.geo-subnetwork.id
-  env          = var.env
-  region       = var.region
-  machine_type = var.machine_type
-}
--- refactored_terraform/tf_state/state_bucket.tf
+++
@@ -1,5 +0,0 @@
-resource "google_storage_bucket" "static-site" {
-  name          = "dev-01-eu-central2-geo-tf-state"
-  location      = "EU"
-  force_destroy = true
-}
