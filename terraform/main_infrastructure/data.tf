500f6ee8c61a72d26b88f691b4b6d45aa45c7c6d
Yura Belichenko <64985383+Yura11@users.noreply.github.com>
2024-06-02 00:25:48 +0300

    Update data.tf

--- terraform/main_infrastructure/data.tf
+++ terraform/main_infrastructure/data.tf
@@ -5,7 +5,3 @@ data "google_compute_network" "vpc_network" {
 data "google_compute_subnetwork" "sub_network" {
   name = "${var.env}-${var.region}-${var.app}-subnet-main"
 }
-
-data "google_compute_subnetwork" "template_subnetwork" {
-  name = "${var.env}-${var.region}-${var.app}-subnet-templates"
-}
