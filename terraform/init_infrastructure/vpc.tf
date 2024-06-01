6bd09c59dbd8dcd8c57f1c9b91b9f66ce1a77976
Yura Belichenko <64985383+Yura11@users.noreply.github.com>
2024-06-01 20:19:18 +0300

    Update vpc.tf

--- terraform/init_infrastructure/vpc.tf
+++ terraform/init_infrastructure/vpc.tf
@@ -7,7 +7,7 @@ resource "google_compute_network" "vpc_network" {
 
 resource "google_compute_subnetwork" "sub_network" {
   name          = "${var.env}-${var.region}-${var.app}-subnet-main"
-  ip_cidr_range = "10.0.0.0/29"
+  ip_cidr_range = "10.2.2.0/29"
   network       = google_compute_network.vpc_network.self_link
 }
 
