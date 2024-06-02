eea67fb96a28bd4c6bf678236ce784bff3689e12
Yura Belichenko <64985383+Yura11@users.noreply.github.com>
2024-06-02 11:26:01 +0300

    Update modules.tf

--- terraform/main_infrastructure/modules.tf
+++ terraform/main_infrastructure/modules.tf
@@ -12,7 +12,7 @@ module "load_balancer_geo" {
   source        = "../modules/load_balancer_geo"
   app           = var.app
   vpc_network   = data.google_compute_network.vpc_network.id
-  sub_network   = google_compute_subnetwork.template_subnetwork.id
+  sub_network   = data.google_compute_subnetwork.sub_network.id
   env           = var.env
   region        = var.region
   instance_type = var.instance_type
