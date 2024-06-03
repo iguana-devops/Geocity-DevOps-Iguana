9214ce4e6b4214d5f4557388169a464784ba0eb5
Yura Belichenko <64985383+Yura11@users.noreply.github.com>
2024-06-03 18:36:43 +0300

    Update autoscale.tf

--- terraform/modules/load_balancer_geo/autoscale.tf
+++ terraform/modules/load_balancer_geo/autoscale.tf
@@ -3,7 +3,7 @@ resource "google_compute_autoscaler" "geo" {
   target = google_compute_instance_group_manager.geo.id
 
   autoscaling_policy {
-    max_replicas    = 3
+    max_replicas    = 2
     min_replicas    = 1
     cooldown_period = 60
 
