4c46f44ce498a21c5c4ec85dee530ceeb86c8561
AlanaSahaidak <sahaidak_alana@ukr.net>
2024-06-10 09:21:13 +0300

    Added variables for Kubernetes cluster

---
+++ terraform/kubernetes_infrastructure/var.tf
@@ -0,0 +1,34 @@
+variable "project" {
+  description = "Google cloud active project"
+  type        = string
+}
+
+variable "env" {
+  description = "The Working environment"
+  type        = string
+}
+
+variable "app" {
+  description = "The application name"
+  type        = string
+}
+
+variable "region" {
+  description = "The default region to deploy infrastructure"
+  type        = string
+}
+
+variable "zone" {
+  description = "The availability zone where the resource will be deployed"
+  type        = string
+}
+
+variable "gke_num_nodes" {
+  default     = 3
+  description = "number of gke nodes"
+}
+
+variable "gke_disk_size" {
+  default     = 10
+  description = "disk size in GB for each node"
+}
