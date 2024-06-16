056d551000e9ca253452cc8e5b48f4489fbafcdf
Andrii Kovalchuk <andriy.p.kovalchuk@gmail.com>
2024-06-16 13:06:55 +0300

    Jenkinsfile changed

--- Jenkins/names.sh
+++ Jenkins/names.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
-file=$1
 
+file=$1
 cat $file | grep '.war' | cut -d: -f 2 | cut -d\" -f 2 > test.txt
-echo -n "[" > names.txt
+#cat /var/lib/jenkins/workspace/artifact/java-app/target/search_output.txt  | grep '.war' | cut -d: -f 2 | cut -d\" -f 2 > test.txt
+echo -n "return[" > names.txt
 while read -r line
 do
    echo -n "$line, " >> names.txt
 done < test.txt
 echo "]" >> names.txt
-
