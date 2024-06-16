220b6c928d23b026519db19f685cf611e4ed14d4
Andrii Kovalchuk <andriy.p.kovalchuk@gmail.com>
2024-06-16 14:26:20 +0300

    Jenkinsfile changed

--- Jenkins/names.sh
+++ Jenkins/names.sh
@@ -5,7 +5,7 @@ cat $file | grep '.war' | cut -d: -f 2 | cut -d\" -f 2 > test.txt
 echo -n "return[" > names.groovy
 while read -r line
 do
-   echo -n "$line, " >> names.groovy
+   echo -n "'$line', " >> names.groovy
 done < test.txt
 echo "]" >> names.groovy
 mv names.groovy /var/lib/jenkins/names.groovy
